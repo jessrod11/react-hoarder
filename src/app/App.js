e78d134b759588b3c3d719dda1cdad18428fe3b9
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-12 09:12:59 -0500

    created first route which contains navbar and landing page

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import Login from '../Login/Login';
-import MyStuff from '../MyStuff/MyStuff';
-import Navbar from '../Navbar/Navbar';
-import Register from '../Register/Register';
-import AllStuff from '../AllStuff/AllStuff';
-import SingleStuff from '../SingleStuff/SingleStuff';
-import Home from '../Home/Home';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+
 import './App.css';
+// import Login from '../Login/Login';
+// import MyStuff from '../MyStuff/MyStuff';
+import Navbar from '../Navbar/Navbar';
+// import Register from '../Register/Register';
+// import AllStuff from '../AllStuff/AllStuff';
+// import SingleStuff from '../SingleStuff/SingleStuff';
+import Home from '../Home/Home';
 
 class App extends Component {
   render () {
     return (
       <div className="App container">
-        <Login />
-        <MyStuff />
-        <Navbar />
-        <Register />
-        <AllStuff />
-        <SingleStuff />
-        <Home />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route path="/" exact component={Home}/>
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
