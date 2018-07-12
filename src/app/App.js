cebffb85f35e375cb64314e7b17bc956ff4a5cbb
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-12 16:14:14 -0500

    added private route to all things

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import React from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 
 import './App.css';
 // import Login from '../Login/Login';
 // import MyStuff from '../MyStuff/MyStuff';
 import Navbar from '../Navbar/Navbar';
 // import Register from '../Register/Register';
-// import AllStuff from '../AllStuff/AllStuff';
+import AllStuff from '../AllStuff/AllStuff';
 // import SingleStuff from '../SingleStuff/SingleStuff';
 import Home from '../Home/Home';
 
-class App extends Component {
+const PrivateRoute = ({component: Component, authed, ...rest}) => {
+  return (
+    <Route {...rest}
+      render ={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
   render () {
     return (
       <div className="App container">
@@ -20,6 +40,11 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route path="/" exact component={Home}/>
+                <PrivateRoute
+                  path="/allstuff"
+                  authed={this.state.authed}
+                  component={AllStuff}
+                />
               </Switch>
             </div>
           </div>
